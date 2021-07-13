f109a158340f76503f4a49a53c52b17c3e27fe8f
Stanislaw <kovalevichstas2@gmail.com>
2021-07-13 16:27:38 +0300

    Comit 3

--- Project2/Files vs Folders/Files vs Folders/Files vs Folders.cpp
+++ Project2/Files vs Folders/Files vs Folders/Files vs Folders.cpp
@@ -1,26 +1,30 @@
 ﻿#include <iostream>
-#include <fstream>
-#include <cstdlib>
+#include <stdio.h>
 #include "string"
-#include <Windows.h>
+#include"iostream"
+
 
 using namespace std;
-FILE* fp;
-int main(int argc, char* argv[])
+struct Point {
+	int x, y;
+};
+bool doOverlap(Point l1, Point r1, Point l2, Point r2)
 {
-
-    WIN32_FIND_DATA FindFileData;
-    HANDLE hf;
-    LPCWSTR s = L"c:\\*.*";
-    hf = FindFirstFile(s, &FindFileData);
-    if (hf != INVALID_HANDLE_VALUE) {
-        do {
-            printf("%S\n", FindFileData.cFileName);
-        } while (FindNextFile(hf, &FindFileData) != 0);
-        FindClose(hf);
-    }
-
-    system("pause");
-    return 0;
+	if (l1.x == r1.x || l1.y == r1.y || l2.x == r2.x || l2.y == r2.y) 
+		return false;
+	if (l1.x >= r2.x || l2.x >= r1.x)
+		return false;
+	if (l1.y >= r2.y || l2.y >= r1.y)
+		return false;
+	return true;
 }
-
+int main()
+{
+	Point l1 = { 1, 1 }, r1 = { 4, 5 };
+	Point l2 = { 4, 5 }, r2 = { 8, 7 };
+	if (doOverlap(l1, r1, l2, r2))
+		printf("Rectangles Overlap");
+	else
+		printf("Rectangles Don't Overlap");
+	return 0;
+}
